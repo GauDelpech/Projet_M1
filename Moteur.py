d25174852eb8926611d70f17ba15e914dba40e8e
GDelpech_PClocal <gauthier.delpech@gmail.com>
2019-03-26 15:48:54 +0100

    code post séance 26/03/2019

--- Moteur.py
+++ Moteur.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class MoteurCC(object):
 
@@ -11,17 +11,70 @@ class MoteurCC(object):
         self.j = j
         self.f = f
 
-    def volttospeed (self, um, omega, dt):
+    def volt_to_speed (self, um, omega, dt):
         i=(um - self.ke * omega)/self.r
         gamma=self.kc*i
         d_omega=(gamma-self.f*omega)/self.j
         return (dt*d_omega+omega)
 
-    def analspeed(self, dt, ttot):
-        t=0
-        omegaTot = []
+    def anal_speed(self, um, t):
+        omega = (1-np.exp((-1/self.j)*(self.f+(self.kc*self.ke/self.r))*t))*(self.kc*um/(self.r*self.f+self.kc*self.ke))
+        return omega
 
-        while t <= ttot:
-            omega = (1- numpy.exp() )
+    def rep_ind(self, omega_init, dt, temps_tot):
+        um = 1
+        t=0
+        temps = [t]
+        ome_num = [omega_init]
+        ome_ana = [omega_init]
+        ome=omega_init
+
+        while t <= temps_tot:
+
+            ome = moteur.volt_to_speed(um, ome, dt)
+            ome_num.append(ome)
+
+            ome_ana.append(moteur.anal_speed(um, t))
+
             t += dt
-            omegaTot.append(omega)
+            temps.append(t)
+
+        plt.plot(temps, ome_ana, 'b')
+        plt.plot(temps, ome_num, 'r')
+        plt.title("Vitesse de rotation calculée analytiquement (bleu) et numériquement (rouge)")
+        plt.xlabel("temps (s)")
+        plt.ylabel("Vitesse de rotation (rad/s)")
+        plt.show()
+
+class Controleur(object):
+
+    def __init__(self, vitesse_desiree=0, vitesse_actuelle=0):
+        self.va = vitesse_actuelle
+        self.vd = vitesse_desiree
+
+    def prop(self, p):
+        eps = self.vd-self.va
+        return p*eps
+
+
+moteur = MoteurCC()
+p = Controleur(10)
+
+t = 0
+dt = 0.01
+ttot = 2
+omega = 0
+ome = [omega]
+temps = [t]
+
+while t <= ttot:
+    um = p.prop(150)
+    omega = moteur.volt_to_speed(um, omega, dt)
+    ome.append(omega)
+
+    p = Controleur(8, omega)
+    t += dt
+    temps.append(t)
+
+plt.plot(temps, ome)
+plt.show()
