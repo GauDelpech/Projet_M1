1e4f2998f88c8f012b58fcd5abce7495a209690e
GDelpech_PClocal <gauthier.delpech@gmail.com>
2019-03-26 22:18:42 +0100

    Fin p/pi

--- Moteur.py
+++ Moteur.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+
 
 class MoteurCC(object):
 
@@ -12,9 +14,9 @@ class MoteurCC(object):
         self.f = f
 
     def volt_to_speed (self, um, omega, dt):
-        i=(um - self.ke * omega)/self.r
-        gamma=self.kc*i
-        d_omega=(gamma-self.f*omega)/self.j
+        i = (um - self.ke * omega)/self.r
+        gamma = self.kc*i
+        d_omega = (gamma-self.f*omega)/self.j
         return (dt*d_omega+omega)
 
     def anal_speed(self, um, t):
@@ -46,35 +48,92 @@ class MoteurCC(object):
         plt.ylabel("Vitesse de rotation (rad/s)")
         plt.show()
 
+
 class Controleur(object):
 
     def __init__(self, vitesse_desiree=0, vitesse_actuelle=0):
         self.va = vitesse_actuelle
         self.vd = vitesse_desiree
+        self.esp = self.vd-self.va
 
     def prop(self, p):
-        eps = self.vd-self.va
-        return p*eps
+        return p*(self.vd-self.va)
+
+    def pi(self, p, i , dt):
+        if i <= 0.00000001:
+            sys.exit('erreur i = 0')
+
+        prop = self.vd - self.va
+
+        self.esp += (self.vd - self.va)
+        inte = (dt/i)*self.esp
+
+        return p*(prop + inte)
+
+    def clear(self):
+        self.esp = 0
+
+
+def Simul_prop(moteur=MoteurCC(), control=Controleur(), p=10, pas=0.1, ttot=2):
+
+    t = 0
+    temps = [t]
+    omega = 0
+    ome = [omega]
+    val_des = [control.vd]
+
+    while t <= ttot:
+        um = control.prop(p)
+        omega = moteur.volt_to_speed(um, omega, dt)
+        ome.append(omega)
+
+        control = Controleur(control.vd, omega)
+        t += pas
+        temps.append(t)
+        val_des.append(control.vd)
+
+    plt.plot(temps, ome, 'b')
+    plt.plot(temps, val_des, 'r')
+    plt.title('Correction proportionnelle (P='+str(p)+ ') de la vitesse du moteur')
+    plt.xlabel("temps (s)")
+    plt.ylabel("Vitesse de rotation (rad/s)")
+    plt.show()
+
+
+def Simul_pi(moteur=MoteurCC(), control=Controleur(), p=10, i=1, pas=0.1, ttot=2):
+
+    t = 0
+    temps = [t]
+    omega = 0
+    ome = [omega]
+    val_des = [control.vd]
+
+    while t <= ttot:
+        um = control.pi(p, i, pas)
+        omega = moteur.volt_to_speed(um, omega, dt)
+        ome.append(omega)
+
+        control = Controleur(control.vd, omega)
+        t += pas
+        temps.append(t)
+        val_des.append(control.vd)
+
+    plt.plot(temps, ome, 'b')
+    plt.plot(temps, val_des, 'r')
+    plt.title('Correction PI (P='+str(p)+ ' et I='+str(i)+') de la vitesse du moteur')
+    plt.xlabel("temps (s)")
+    plt.ylabel("Vitesse de rotation (rad/s)")
+    plt.show()
+
+    control.clear()
 
 
 moteur = MoteurCC()
-p = Controleur(10)
+contr = Controleur(10)
+P = 900
+I = 0.1
+dt = 0.001
+ttot = 0.1
+Simul_pi(moteur, contr, P, I, dt, ttot)
+# Simul_prop(moteur, contr, P, dt, ttot)
 
-t = 0
-dt = 0.01
-ttot = 2
-omega = 0
-ome = [omega]
-temps = [t]
-
-while t <= ttot:
-    um = p.prop(150)
-    omega = moteur.volt_to_speed(um, omega, dt)
-    ome.append(omega)
-
-    p = Controleur(8, omega)
-    t += dt
-    temps.append(t)
-
-plt.plot(temps, ome)
-plt.show()
